f4a81379e0fda83bce580e5737d4f7ef33df0298
Clark Nelson <clark@cnsoft-online.com>
2026-09-14 09:48:08 +0100

    v2.0.9 PIP packages update

--- CNSoft.WeatherPi.Readings.V2/readings.py
+++ CNSoft.WeatherPi.Readings.V2/readings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-WeatherPI Version 2.0.8 - Sensor Readings Publisher
+WeatherPI Version 2.0.9 - Sensor Readings Publisher
 Author: Clark Nelson
 Company: CNSoft
 Copyright (c) 2026 CNSoft. All rights reserved.
